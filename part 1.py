c800fc598b3850f63bc9a8251b627866859ec932
TenHag <108280659+TenHag@users.noreply.github.com>
2023-02-23 19:40:23 +0530

    Add files via upload

---
+++ part 1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM,Dense, Dropout, SpatialDropout1D
+from tensorflow.keras.layers import Embedding
+
+df = pd.read_csv("./Tweets.csv")
+
+
+df.head()
+df.columns
+
+tweet_df = df[['text','airline_sentiment']]
+print(tweet_df.shape)
+tweet_df.head(5)
+
+
+tweet_df = tweet_df[tweet_df['airline_sentiment'] != 'neutral']
+print(tweet_df.shape)
+tweet_df.head(5)
+
+tweet_df = tweet_df[tweet_df['airline_sentiment'] != 'neutral']
+print(tweet_df.shape)
+tweet_df.head(5)
+
+
+tweet_df["airline_sentiment"].value_counts()
+
+sentiment_label = tweet_df.airline_sentiment.factorize()
+sentiment_label
+
+
+tweet = tweet_df.text.values
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(tweet)
+vocab_size = len(tokenizer.word_index) + 1
+encoded_docs = tokenizer.texts_to_sequences(tweet)
+padded_sequence = pad_sequences(encoded_docs, maxlen=200)
+
